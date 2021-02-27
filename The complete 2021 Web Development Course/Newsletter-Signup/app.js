1c1276c2fa19c33a22d1f260b7f030ba0bc7595b
Joshua Jacob Mauricio <jmauricio15@wou.edu>
2021-02-27 01:25:17 -0800

    finishing second part of setting up news letter

--- The complete 2021 Web Development Course/Newsletter-Signup/app.js
+++ The complete 2021 Web Development Course/Newsletter-Signup/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+const https = require('https');
 
 const app = express();
 
@@ -17,10 +18,56 @@ app.get('/', function(req, res){
 });
 
 app.post('/', function(req, res){
-    var firstName = req.body.firstName;
-    var lastName = req.body.lastName;
-    var email = req.body.email;
-    console.log(firstName + " " + lastName + " " + email);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    //console.log(firstName + " " + lastName + " " + email);
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    //turning data into a flatpack version of JSON
+    const jsonData = JSON.stringify(data);
+
+    const url =   'https://us1.api.mailchimp.com/3.0/lists/{list_id}';
+
+    const options = {
+        method: "POST",
+        auth: "joshmdev:{api_key}"
+    }
+
+    //basic part of the post request we want to send to mailchimp
+    const request = https.request(url, options, function(response){
+        
+        //depending on If the request worked, we want to return certain a response
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + '/success.html');
+        }
+        else{
+            res.sendFile(__dirname_ + '/failure.html');
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        });
+    });
+
+    //including the data of the person we would like to add
+    //This is like adding to the parameters of a request
+    request.write(jsonData);
+    request.end();
 });
 
-
+app.post('/failure', function(req, res){
+    res.redirect("/");
+})
